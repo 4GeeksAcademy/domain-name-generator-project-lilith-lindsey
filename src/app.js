44f6c573dfbda00ccbe6352597b1b86f9acd09c5
Lilith Lindsey <lilithx1313@gmail.com>
2025-07-21 18:39:54 +0000

    Confirmed necessary changes and functionality.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import "bootstrap";
 import "./style.css";
 
+const pronoun = ['the', 'our', 'my', 'his'];
+const adj = ['big', 'glorious', 'crazy', 'terrifying'];
+const noun = ['racoon', 'robot', 'sandwich', 'astronaut'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const myURL = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++){
+    for (let j = 0; j < adj.length; j++){
+        for (let k = 0; k < noun.length; k++){
+            const domain = [pronoun[i] + adj[j] + noun[k]] + ".com";
+            myURL.push(domain)
+        }
+    }
+}
+
+console.log(myURL)
