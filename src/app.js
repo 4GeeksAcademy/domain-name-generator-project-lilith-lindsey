1134f37a3f90c8674de8ac13f6ce1f7f0d4e210f
Lilith Lindsey <lilithx1313@gmail.com>
2025-08-17 20:29:04 +0000

    Updated to fit the generative code within  a function that can be reused

--- src/app.js
+++ src/app.js
@@ -5,15 +5,19 @@ const pronoun = ['the', 'our', 'my', 'his'];
 const adj = ['big', 'glorious', 'crazy', 'terrifying'];
 const noun = ['racoon', 'robot', 'sandwich', 'astronaut'];
 
-const myURL = [];
 
-for (let i = 0; i < pronoun.length; i++){
-    for (let j = 0; j < adj.length; j++){
-        for (let k = 0; k < noun.length; k++){
-            const domain = [pronoun[i] + adj[j] + noun[k]] + ".com";
-            myURL.push(domain)
+function generateDomains(pronoun, adj, noun) {
+    const myURL = [];
+    for (let i = 0; i < pronoun.length; i++){
+        for (let j = 0; j < adj.length; j++){
+            for (let k = 0; k < noun.length; k++){
+                const domain = pronoun[i] + adj[j] + noun[k] + ".com";
+                myURL.push(domain)
+            }
         }
     }
+    return myURL;
 }
 
-console.log(myURL)
+const domains = generateDomains(pronoun, adj, noun);
+console.log(domains);
